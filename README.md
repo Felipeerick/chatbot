88e6e31ae9a7a88d421f0943ef8a8a40537a843a
Felipe Erick <rick0531927@gmail.com>
2023-05-08 20:54:10 -0300

    ajustes no código

--- README.md
+++ README.md
@@ -7,22 +7,8 @@
     <a href="https://nodejs.org/en/">🔗 NodeJS</a>
     <a href="https://www.npmjs.com/package/venom-bot">🧠 Venom Bot</a>
 </h1>
-<p align="center">🚀 Projeto criado com o intuito de auxiliar nas demandas de pedidos da empresa "Delícias da Neide" via WhatsApp.</p>
+<p align="center">🚀 Projeto criado com o intuito de auxiliar nas demandas da empresa "Amoedo Créditos" via WhatsApp.</p>
 <br>
-<h1 align="center">
-  Welcome
-  <img alt="Welcome" title="Welcome" src="./assets/welcome.png" />
-  Menu
-  <img alt="Menu" title="Menu" src="./assets/menu.png" />
-  Order
-  <img alt="Order" title="Order" src="./assets/order.png" />
-  Address
-  <img alt="Address" title="Address" src="./assets/address.png" />
-  Bill
-  <img alt="Bill" title="Bill" src="./assets/bill.png" />
-  Assistant
-  <img alt="Assistant" title="Assistant" src="./assets/assistant.png" />
-</h1>
 
 ### Pré-requisitos
 
@@ -34,8 +20,7 @@ Além disto, é bom ter um editor para trabalhar com o código como [VSCode](htt
 
 ```bash
 # Clone este repositório
-$ git clone git@github.com:juniorwmr/botwhatsapp-venom.git
-
+$ git clone https://github.com/Felipeerick/chatbot.git
 # Acesse a pasta do projeto no terminal/cmd
 $ cd botwhatsapp-venom
 
@@ -56,13 +41,4 @@ As seguintes ferramentas foram usadas na construção do projeto:
 
 ### Autor
 
----
-
-<a href="https://juniorwmr.github.io/">
- <img style="border-radius: 30;" src="https://juniorwmr.github.io/images/washington-muniz.jpg" width="100px;" alt=""/>
- <br />
-
-Done with ❤️ by Washington Muniz 👋🏽 !
-
-[![Twitter Badge](https://img.shields.io/badge/-@juniorwmr-1ca0f1?style=flat-square&labelColor=1ca0f1&logo=twitter&logoColor=white&link=https://twitter.com/juniorwmr)](https://twitter.com/juniorwmr) [![Linkedin Badge](https://img.shields.io/badge/-Washington-blue?style=flat-square&logo=Linkedin&logoColor=white&link=https://www.linkedin.com/in/juniorwmr/)](https://www.linkedin.com/in/juniorwmr/)
-[![Gmail Badge](https://img.shields.io/badge/-juniorwmr@gmail.com-c14438?style=flat-square&logo=Gmail&logoColor=white&link=mailto:juniorripardo@gmail.com)](mailto:juniorripardo@gmail.com)
+Done with ❤️ by Felipe Erick 👋🏽 !
--- src/server.js
+++ src/server.js
@@ -1,21 +1,90 @@
 import { create } from 'venom-bot';
-import methods from './methods.js';
+
+const hoje = new Date();
 
 create({
-    session: 'store',
-    multidevice: true,
-    headless: false,
-  })
+  session: 'store',
+  headless: false,
+})
   .then((client) => start(client))
   .catch((error) => {
     console.error(error);
     process.exit(1);
   });
 
-  function start(client) {
-    client.onMessage((message) => {
-      if (!message.isGroupMsg) {
-        methods.handleConversation(client, message);
+ function takeToday(client){
+    if (hoje.getDay() != 0) {
+      client.setProfileStatus('Estamos abertos!');
+      return true;
+    } else {
+      client.setProfileStatus('Estamos fechados!');
+      return false;
+    }
+  };
+
+  function responseToTheImpatientUser(client, message){
+    let attempts = 0;
+    const interval = setInterval(() => {
+      if (attempts >= 3) {
+        clearInterval(interval);
+        client.sendText(message.from, 'Estamos procurando um atendente, espere alguns minutos.');
+        attempts = 0;
+        setTimeout(() => {
+          client.sendText(message.from, 'Posso ajudar em algo mais? Selecione uma opção: (4) - Voltar para o menu principal, (5) - Finalizar atendimento');
+        }, 120000); // espera 2 minutos
+      } else {
+        attempts++;
+        client.sendText(message.from, 'Estamos procurando um atendente, espere alguns minutos.');
       }
-    });
-}
+    }, 5000); // espera 5 segundos entre cada tentativa
+  }
+
+function start(client) {
+  let workingDay = takeToday(client);
+  let listenerId;
+  let messageFrom;
+  client.onMessage((message) => {
+    messageFrom = message.from;
+    console.log(message, "objeto da mensagem\n\n");
+    console.log(message.from.count() < 18, "boolean");
+    
+
+    if (message.isGroupMsg === false && message.chat.isGroup === false && messageFrom.length < 18) {
+      if (!workingDay) {
+        client.sendText(message.from, 'Estamos fechados. Horário de funcionamento somente das 8 as 18 horas, de segunda a sábado.');
+      } else {
+            // console.log(message, "messagem comum"),
+            // console.assert("=================================="),
+
+            if(listenerId == null || undefined || '' && listenerId != message.id){
+              client.sendText(message.from, 'Olá, bem-vindo à Nandinha! Em que posso ajudar? Selecione uma opção: (1) => Formas de contato, (2) => Falar com atendente, (3) => Finalizar atendimento');
+              listenerId = message.id;
+              console.log("Valor do listernerId é: ", listenerId);
+            }
+            
+            switch (message.body) {
+              case '1':
+                client.sendText(message.from, 'Nosso número fictício é (11) 9999-9999. Posso ajudar em algo mais? Selecione uma opção: 4 - Voltar para o menu principal, 5 - Finalizar atendimento');
+                break;
+              case '2':
+                client.sendText(message.from, 'Aguarde um momento, estamos chamando nossos atendentes.');
+                //responseToTheImpatientUser(client, message);
+                break;
+              case '3':
+                client.sendText(message.from, 'Obrigado por entrar em contato com a Nandinha. Volte sempre!');
+                listenerId = null;
+                break;
+              case '4':
+                client.sendText(message.from, 'Selecione uma opção: 1 - Formas de contato, 2 - Falar com atendente, 3 - Finalizar atendimento');
+                break;
+              case '5':
+                client.sendText(message.from, 'Obrigado por entrar em contato com a Nandinha. Volte sempre!');
+                listenerId = null;
+                break;
+              // default:
+              //   client.sendText(message.from, 'Opção inválida. Selecione uma opção: 1 - Formas de contato, 2 - Falar com atendente, 3 - Finalizar atendimento');
+            }
+        };
+      };
+  });
+};
